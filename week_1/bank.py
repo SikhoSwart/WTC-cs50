51025c97cf313ab94180b6aa6e1a8805533c1bda
SikhoSwart <sikhoswart@gmail.com>
2025-07-15 11:58:36 +0200

    bank.py : prompts user for a greetingand assign an amount of money

---
+++ week_1/bank.py
@@ -0,0 +1,15 @@
+#Store greeting in a variable
+#strip removes any leading whitespace
+greeting = input("Greeting: ").strip()
+
+#check if greeting is "hello", lower makes it case sensitive
+if greeting.lower() == "hello":
+    print("$0")
+
+#check if the first letter is h
+elif greeting[0].lower() == "h":
+    print("$20")
+
+#any other greeting
+else:
+    print("$100")
