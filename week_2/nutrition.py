0def242a60dda5d9134c516a2fa50076dd19fb3a
SikhoSwart <sikhoswart@gmail.com>
2025-07-24 20:56:03 +0200

    prompts users to input a fruit then outputs the number of calories

---
+++ week_2/nutrition.py
@@ -0,0 +1,18 @@
+#dictionary for fruits and their calories
+fruit = {
+    "Apple": 130, "Avocado": 50,
+    "Banana": 110, "Cantaloupe": 50,
+    "Grapefruit": 60, "Grapes": 90,
+    "Honeydew Melon": 50, "Kiwifruit": 90,
+    "Lemon": 15, "Lime": 20,
+    "Nectarine": 60, "Orange": 80,
+    "Peach": 60, "Pear": 100,
+    "Pineapple": 50, "Plums": 70,
+    "Strawberries": 50, "Sweet Cherries": 100,
+    "Tangerine": 50, "Watermelon": 80
+}
+
+calories = input("Item: ")
+
+if calories in fruit:
+    print("Calories: ", fruit[calories])
