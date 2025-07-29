b1d22a3d329010f99eeca49e04605f36410c3954
SikhoSwart <sikhoswart@gmail.com>
2025-07-29 15:36:48 +0200

    indicate, from fractions, just how much fuel is in a tank

---
+++ week_3/fuel.py
@@ -0,0 +1,35 @@
+#while loop to prompt user again if exceptions are raised
+while True:
+    try:
+        #prompt user for fraction formatted as X/Y
+        frac = input("Fraction: ")
+        #separate the fraction to get each integer
+        frac = frac.split("/")
+        #Store eeach integer in a variable
+        x = int(frac[0])
+        y = int(frac[1])
+        #calculate the percentage
+        perc = int((x/y)*100)
+
+    #handle exceptions
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+    #handle cases where x>y and 'x/y' < 0
+    if x > y:
+        continue
+    if (x/y) < 0:
+        continue
+
+    else:
+        #1% or less remains, output E 
+        if perc <= 1:
+            print("E")
+        #if 99% or more remains, output F
+        elif perc >= 99:
+            print("F")
+        else:
+            print(f'{perc}%')
+
+        break
